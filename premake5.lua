df68b135625e508874d71d9444ee21b15d7598d8
Mouradouchane <mouradouchane@gmail.com>
2024-07-04 12:25:11 +0100

    add:includes and linking configs to premake script

--- premake5.lua
+++ premake5.lua
@@ -14,6 +14,11 @@ project(projects[i])
    kind "ConsoleApp"
    language "C++"
    
+   includedirs { "libs" }
+
+   links{ "libs/glew/glew32s.lib" }
+   links{ "libs/glfw/glfw3.lib" }
+
    files { 
 	projects[i] .. "/**.h", 
 	projects[i] .. "/**.hpp" , 
